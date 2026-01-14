680a38e7a91dbed36389eb0e7df4b0eb2ec06968
Jai Pandey <jaipandey743@gmail.com>
2026-01-14 16:17:18 +0530

    added division files

---
+++ division.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 21;
+    int b = 10;
+
+    printf("21 when divided by 10 leaves the remainder %d\n", a % b);
+    // Expected output: 21 when divided by 10 leaves the remainder 1
+
+    /* Explanation: The modulus operator (%) gives the remainder of the
+    division of a by b. */
+    
+    return 0;
+
+}
