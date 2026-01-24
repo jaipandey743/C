8290b718db32fe317f08ee8adbb545076ae425a4
Jai Pandey <jaipandey743@gmail.com>
2026-01-24 17:00:24 +0530

    Added file related to ternary operator

---
+++ ternery.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+ 
+int main(){
+
+int a;
+
+printf("Enter the value of a\n");
+scanf("%d",&a);
+
+(a <= 5)? printf("a is less than 5\n"):printf("a is greater than 5\n");
+
+    return 0;
+}
