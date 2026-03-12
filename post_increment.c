d3be9719609ef50eddc69307906b57edd2243ef7
Jai Pandey <jaipandey743@gmail.com>
2026-03-12 14:20:35 +0530

    Pre increment operator

---
+++ post_increment.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int a=6;
+    printf("The value of a is %d\n",a);
+    printf("The value of a++ is %d\n",a++);
+    printf("The value of a is %d\n",a);
+
+
+    return 0;
+}
