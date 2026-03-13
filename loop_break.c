80607e927bdeffb3a967126c88ec7de6c6573104
Jai Pandey <jaipandey743@gmail.com>
2026-03-13 18:19:22 +0530

    loop break

---
+++ loop_break.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int i;
+    for(i=0;i<10;i++)
+    {
+        printf("The value of i is %d\n",i);
+        if(i==5)
+        {
+            break;
+        }   
+    }
+        return 0;
+}
