08cee9506d898e826e89a010f7c831535886d3d4
Jai Pandey <jaipandey743@gmail.com>
2026-03-14 13:32:41 +0530

    Basic functions in C

---
+++ basic_functions.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void Goodmorning();
+void Goodafternoon();
+void Goodevening();
+int main(){
+     Goodmorning();
+    Goodafternoon();
+     Goodevening();
+     return 0; 
+}
+void Goodmorning(){
+printf("Good morning\n");
+}
+void Goodafternoon(){
+printf("Good afternoon\n");
+}
+void Goodevening(){
+printf("Good evening\n");
+}
+
